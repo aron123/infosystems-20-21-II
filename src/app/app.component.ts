2ab42a7deebbd96e33f2aee7c942a29dcb7c84ed
Kiss, Aron <aron123dev@gmail.com>
2021-02-13 14:11:50 +0100

    Add example of Bootstrap styling, structural directives, dynamic behaviours and two-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infosystems';
+  items = [ 'item1', 'item2', 'item3' ];
+  objects = [
+    { id: 1, title: 'title1', description: 'sdasd'},
+    { id: 2, title: 'title2', description: 'asdasdsa' },
+    { id: 3, title: 'title3', description: 'asdasdasdas' },
+  ]
+  showSecretText = false;
+
+  removeLastItem () {
+    this.items.pop();
+  }
+
+  removeFirstObject() {
+    this.objects.shift();
+  }
+
+  toggleSecretText() {
+    this.showSecretText = !this.showSecretText;
+  }
 }
