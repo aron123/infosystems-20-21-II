995b378d51da652c277756b0df209c52d7c28223
Kiss, Aron <aron123dev@gmail.com>
2021-02-14 13:13:08 +0100

    Add semantic versioning example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infosystems';
+  major: number = 0;
+  minor: number = 0;
+  patch: number = 1;
+  changelog = [
+    { type: 'patch', old: 0, new: 1 }
+  ];
+
+  incrementMajor() {
+    this.major++;
+    this.minor = 0;
+    this.patch = 0;
+    this.changelog.push({ type: 'major', old: this.major - 1, new: this.major });
+  }
+
+  incrementMinor() {
+    this.minor++;
+    this.patch = 0;
+    this.changelog.push({ type: 'minor', old: this.minor - 1, new: this.minor });
+  }
+
+  incrementPatch() {
+    this.patch++;
+    this.changelog.push({ type: 'patch', old: this.patch - 1, new: this.patch });
+  }
+
 }
