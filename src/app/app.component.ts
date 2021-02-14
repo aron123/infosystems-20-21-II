9e8de0be82377b23388c6f1e503db3be480acee7
Kiss, Aron <aron123dev@gmail.com>
2021-02-14 13:28:37 +0100

    Add voting example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'infosystems';
+  agreeCount = 0;
+  disagreeCount = 0;
+  people = [
+    { name: 'Mr. IQ', voted: false },
+    { name: 'Ms. Universe', voted: false },
+    { name: 'Bombasto', voted: false }
+  ];
+
+  vote(index: number, agree: boolean) {
+    this.people[index].voted = true;
+
+    if (agree) {
+      this.agreeCount++;
+    } else {
+      this.disagreeCount++;
+    }
+    
+  }
 }
